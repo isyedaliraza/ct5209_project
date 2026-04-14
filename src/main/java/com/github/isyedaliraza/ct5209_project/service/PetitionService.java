89b5f9ce7fbdd38b8a8bebbd361f71e6a3303698
Syed Ali Raza <isyedaliraza@gmail.com>
2026-04-14 17:00:44 +0400

    Add petition Service

---
+++ src/main/java/com/github/isyedaliraza/ct5209_project/service/PetitionService.java
@@ -0,0 +1,34 @@
+package com.github.isyedaliraza.ct5209_project.service;
+
+import com.github.isyedaliraza.ct5209_project.domain.Petition;
+import com.github.isyedaliraza.ct5209_project.domain.PetitionRepository;
+import com.github.isyedaliraza.ct5209_project.model.PetitionDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PetitionService {
+    private final PetitionRepository petitionRepository;
+
+    public PetitionService(PetitionRepository petitionRepository) {
+        this.petitionRepository = petitionRepository;
+    }
+
+    public List<PetitionDTO> findAll() {
+        List<PetitionDTO> petitions = new ArrayList<>();
+        for (Petition petition : petitionRepository.findAll()) {
+            petitions.add(new PetitionDTO(petition));
+        }
+        return petitions;
+    }
+
+    public List<PetitionDTO> findByTitle(String q) {
+        List<PetitionDTO> petitions = new ArrayList<>();
+        for (Petition petition: petitionRepository.findByTitleContainingIgnoreCase(q)) {
+            petitions.add(new PetitionDTO(petition));
+        }
+        return petitions;
+    }
+}
