7b7ad1d875860dd79fc51b034decc95f5472da23
Syed Ali Raza <isyedaliraza@gmail.com>
2026-04-14 17:02:40 +0400

    View all petitions

--- src/main/java/com/github/isyedaliraza/ct5209_project/controller/PetitionsController.java
+++ src/main/java/com/github/isyedaliraza/ct5209_project/controller/PetitionsController.java
@@ -1,14 +1,27 @@
 package com.github.isyedaliraza.ct5209_project.controller;
 
+import com.github.isyedaliraza.ct5209_project.model.PetitionDTO;
+import com.github.isyedaliraza.ct5209_project.service.PetitionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PetitionsController {
+    private final PetitionService petitionService;
+
+    public PetitionsController(PetitionService petitionService) {
+        this.petitionService = petitionService;
+    }
+
     @GetMapping
     public String index(Model model) {
+        List<PetitionDTO> petitions = petitionService.findAll();
         model.addAttribute("title", "Petitions | Home");
+        model.addAttribute("petitions", petitions);
         return "index";
     }
 
