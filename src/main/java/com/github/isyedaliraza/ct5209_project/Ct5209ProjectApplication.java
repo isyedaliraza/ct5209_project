f2be54322208dcf19db8883f29a1bd62303a1ba5
Syed Ali Raza <isyedaliraza@gmail.com>
2026-04-14 14:20:47 +0400

    First commit

---
+++ src/main/java/com/github/isyedaliraza/ct5209_project/Ct5209ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.isyedaliraza.ct5209_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ct5209ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ct5209ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/isyedaliraza/ct5209_project/Ct5209ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.isyedaliraza.ct5209_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ct5209ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
