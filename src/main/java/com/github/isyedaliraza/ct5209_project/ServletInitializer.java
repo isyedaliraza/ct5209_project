845f7465ff19dfdef77b8e3bc6730388f62e47ef
Syed Ali Raza <isyedaliraza@gmail.com>
2026-04-15 13:04:33 +0400

    Change packaging type to war

---
+++ src/main/java/com/github/isyedaliraza/ct5209_project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.github.isyedaliraza.ct5209_project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Ct5209ProjectApplication.class);
+    }
+
+}
